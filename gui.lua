66c1b7d2812760f25f9c5f6e887593bba9cb0341
Lisso-lab <85261069+Lisso-lab@users.noreply.github.com>
2022-09-24 01:42:28 -0700

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -1,47 +1,47 @@
-local run_service,tween_service,user_input_service = game:GetService('RunService'),game:GetService('TweenService'),game:GetService('UserInputService')
-local plr = game:GetService('Players').LocalPlayer
+--!strict
+-- I know this UI sucks...
 
-local drag,id_input,method_input,speed_input,play_bttn = loadstring(game:HttpGet('https://raw.githubusercontent.com/Lisso-lab/Animation-ID-player/main/gui.lua'))() --
-
-local motors = {}
-
-local methods = {
-    ['Tween'] = 4,
-    ['Lerp'] = 2,
-    ['CFrame'] = 0
-}
-
-local settings = {
-    ['Method'] = 'Tween',
-    ['Animation ID'] = '4846912064',
-    ['Speed'] = .04
-}
-
-_G.run_player = false
-wait(.5)
-_G.run_player = true
-
+local ScreenGui = Instance.new("ScreenGui")
+local TextLabel = Instance.new("TextLabel")
+local Main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local IDinput = Instance.new("TextBox")
+local UICorner_2 = Instance.new("UICorner")
+local IDhere = Instance.new("TextButton")
+local ModeTweenCFrameLerp = Instance.new("TextButton")
+local ModeInput = Instance.new("TextBox")
+local UICorner_3 = Instance.new("UICorner")
+local Speed = Instance.new("TextButton")
+local PlayButton = Instance.new("TextButton")
+local UICorner_4 = Instance.new("UICorner")
+local SpeedInput = Instance.new("TextBox")
+local UICorner_5 = Instance.new("UICorner")
 
 local function make_draggable(gui)
-    local dragging,drag_input,drag_start,start_pos,last_mouse_pos,last_goal_pos
-    
-    local drag_speed = 20
+    local user_input_service: UserInputService = game:GetService("UserInputService")
+    local run_service: RunService = game:GetService("RunService")
 
-    local function lerp(a, b, m)
+    local drag_start,last_goal_pos,start_pos,last_mouse_pos: UDim2
+    local dragging: boolean
+    local drag_input --Cant do enum in typechecking cuz exploits....
+
+    local drag_speed: number = 20
+
+    local function lerp(a: number, b: number, m: number): number
     	return a + (b - a) * m
     end
 
-    local function update(dt)
+    local function update(dt: number)
     	if not start_pos then return end
 
         if not dragging and last_goal_pos then
             gui.Position = UDim2.new(start_pos.X.Scale,lerp(gui.Position.X.Offset,last_goal_pos.X.Offset,dt * drag_speed),start_pos.Y.Scale,lerp(gui.Position.Y.Offset,last_goal_pos.Y.Offset,dt * drag_speed))
 
-            return 
+            return
         end
 
-        local delta = (last_mouse_pos - user_input_service:GetMouseLocation())
-        local x_goal,y_goal = (start_pos.X.Offset - delta.X),(start_pos.Y.Offset - delta.Y)
+        local delta: Vector3 = (last_mouse_pos - user_input_service:GetMouseLocation())
+        local x_goal,y_goal: number = (start_pos.X.Offset - delta.X),(start_pos.Y.Offset - delta.Y)
 
         last_goal_pos = UDim2.new(
             start_pos.X.Scale, x_goal,
@@ -71,166 +71,131 @@ local function make_draggable(gui)
             drag_input = input
         end
     end)
-    
-    run_service['RenderStepped']:Connect(update)
+
+    run_service["RenderStepped"]:Connect(update)
 end
 
-local function get_animation(id)
-    return game:GetObjects('rbxassetid://' ..id)[1]
-end
+ScreenGui.Name = 'Animation ID Player'
+ScreenGui.Parent = game.CoreGui
 
-local function on_focus_lost_id()
-    local text = id_input.Text
+TextLabel.AnchorPoint = Vector2.new(0, 0.5)
+TextLabel.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
+TextLabel.BackgroundTransparency = 1.000
+TextLabel.BorderColor3 = Color3.fromRGB(27, 42, 53)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.534934461, 0, 0.370528102, 0)
+TextLabel.Size = UDim2.new(0, 206, 0, 32)
+TextLabel.ZIndex = 2
+TextLabel.Font = Enum.Font.Ubuntu
+TextLabel.Text = "Animation ID player"
+TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.TextSize = 18.000
+TextLabel.Parent = ScreenGui
 
-    text = string.gsub(text,'%a%p%s','')
-    print(text)
+Main.Name = "Main"
+Main.AnchorPoint = Vector2.new(0, 0.5)
+Main.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Main.Position = UDim2.new(-0.000584870868, 0, 4.13782978, 0)
+Main.Size = UDim2.new(0, 206, 0, 265)
+Main.Parent = TextLabel
 
-    if string.len(text) < 7 then
-        id_input.Text = '4846912064'
-    else
-        id_input.Text = text
-    end
+UICorner.Parent = Main
 
-    settings['Animation ID'] = id_input.Text
-end
+IDinput.Name = "ID input"
+IDinput.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+IDinput.BorderSizePixel = 0
+IDinput.Position = UDim2.new(0.0291262139, 0, 0.273958951, 0)
+IDinput.Size = UDim2.new(0, 193, 0, 24)
+IDinput.ZIndex = 2
+IDinput.Font = Enum.Font.SourceSans
+IDinput.PlaceholderText = "ID here"
+IDinput.Text = "4846912064"
+IDinput.TextColor3 = Color3.fromRGB(255, 255, 255)
+IDinput.TextSize = 14.000
+IDinput.Parent = Main
 
-local function on_focus_lost_method()
-    local text = method_input.text
-    print(text)
+UICorner_2.CornerRadius = UDim.new(0, 3)
+UICorner_2.Parent = IDinput
 
-    if not methods[text] then
-        method_input.Text = 'Tween'
-    else
-        method_input.Text = text
-    end
+IDhere.Name = "ID here:"
+IDhere.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+IDhere.BackgroundTransparency = 1.000
+IDhere.Position = UDim2.new(0.0268791709, 0, 0.198967218, 0)
+IDhere.Size = UDim2.new(0, 57, 0, 20)
+IDhere.Font = Enum.Font.Ubuntu
+IDhere.Text = "ID here:"
+IDhere.TextColor3 = Color3.fromRGB(255, 255, 255)
+IDhere.TextSize = 15.000
+IDhere.Parent = Main
 
-    settings['Method'] = method_input.Text
-end
+ModeTweenCFrameLerp.Name = "Mode: (Tween,CFrame,Lerp)"
+ModeTweenCFrameLerp.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ModeTweenCFrameLerp.BackgroundTransparency = 1.000
+ModeTweenCFrameLerp.Position = UDim2.new(0.0221456885, 0, 0.396591127, 0)
+ModeTweenCFrameLerp.Size = UDim2.new(0, 186, 0, 25)
+ModeTweenCFrameLerp.Font = Enum.Font.Ubuntu
+ModeTweenCFrameLerp.Text = "Mode: (Tween,CFrame,Lerp)"
+ModeTweenCFrameLerp.TextColor3 = Color3.fromRGB(255, 255, 255)
+ModeTweenCFrameLerp.TextSize = 15.000
+ModeTweenCFrameLerp.Parent = Main
 
-local function on_focus_lost_speed()
-    local num = tonumber(speed_input.Text)
-    print(num)
+ModeInput.Name = "Mode Input"
+ModeInput.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+ModeInput.BorderSizePixel = 0
+ModeInput.Position = UDim2.new(0.0291262139, 0, 0.489179492, 0)
+ModeInput.Size = UDim2.new(0, 193, 0, 24)
+ModeInput.ZIndex = 2
+ModeInput.Font = Enum.Font.SourceSans
+ModeInput.PlaceholderColor3 = Color3.fromRGB(178, 178, 178)
+ModeInput.PlaceholderText = "Mode here"
+ModeInput.Text = "tween"
+ModeInput.TextColor3 = Color3.fromRGB(255, 255, 255)
+ModeInput.TextSize = 14.000
+ModeInput.Parent = Main
 
-    if not num then
-        speed_input.Text = .03
-    else
-        speed_input.Text =  num
-    end
+UICorner_3.CornerRadius = UDim.new(0, 3)
+UICorner_3.Parent = ModeInput
 
-    settings['Speed'] = tonumber(speed_input.Text)
-end
+Speed.Name = "Speed:"
+Speed.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Speed.BackgroundTransparency = 1.000
+Speed.Position = UDim2.new(0.0268791709, 0, 0.602740765, 0)
+Speed.Size = UDim2.new(0, 57, 0, 20)
+Speed.Font = Enum.Font.Ubuntu
+Speed.Text = "Speed: "
+Speed.TextColor3 = Color3.fromRGB(255, 255, 255)
+Speed.TextSize = 15.000
+Speed.Parent = Main
 
-local swait do
-    local stack = 0
-    local step = run_service['RenderStepped']:wait()
+PlayButton.Name = "Play Button"
+PlayButton.AnchorPoint = Vector2.new(0, 0.5)
+PlayButton.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+PlayButton.Position = UDim2.new(0.0291262139, 0, 0.901886821, 0)
+PlayButton.Size = UDim2.new(0, 193, 0, 40)
+PlayButton.Font = Enum.Font.SourceSans
+PlayButton.Text = "Play"
+PlayButton.TextColor3 = Color3.fromRGB(255, 255, 255)
+PlayButton.TextSize = 30.000
+PlayButton.Parent = Main
 
-    function swait(times)
-        if times then
-            local total = 0
-            stack += times
+UICorner_4.Parent = PlayButton
 
-            while stack >= step do
-                step = run_service['RenderStepped']:wait()
-                
-                stack -= step
-                total += step
-            end
-        else
-            return run_service['RenderStepped']:wait()
-        end
-    end
-end
+SpeedInput.Name = "Speed Input"
+SpeedInput.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
+SpeedInput.BorderSizePixel = 0
+SpeedInput.Position = UDim2.new(0.0291262139, 0, 0.677732527, 0)
+SpeedInput.Size = UDim2.new(0, 193, 0, 24)
+SpeedInput.ZIndex = 2
+SpeedInput.Font = Enum.Font.SourceSans
+SpeedInput.PlaceholderText = "0.03"
+SpeedInput.Text = "0.03"
+SpeedInput.TextColor3 = Color3.fromRGB(255, 255, 255)
+SpeedInput.TextSize = 14.000
+SpeedInput.Parent = Main
 
-local function play()
-    local animation = get_animation(settings['Animation ID'])
+UICorner_5.CornerRadius = UDim.new(0, 3)
+UICorner_5.Parent = SpeedInput
 
-    local char = plr.Character or plr.CharacterAdded:wait()
-    local hum = char:FindFirstChildWhichIsA("Humanoid")
+make_draggable(TextLabel)
 
-    local motors = {}
-    
-    for _,v in pairs(char:GetDescendants()) do
-        if v:IsA('Motor6D') then motors[#motors+1] = v end
-    end
-
-    for _, anim_tracks in pairs(hum:GetPlayingAnimationTracks()) do
-        anim_tracks:Stop()
-    end
-    
-    do local animate: LocalScript = char:FindFirstChild("Animate")
-        if animate then animate.Disabled = true end
-    end
-    
-    if playing then
-        playing = false
-        wait(.5)
-    end
-
-    playing = true
-
-    while wait() and playing do
-        for _,frame in pairs(animation:GetKeyframes()) do
-            for _,v in pairs(frame:GetDescendants()) do
-    
-                local current_motor do
-                    for i=1,#motors do
-                        if motors[i].Part1.Name ~= v.Name then continue end
-    
-                        current_motor = motors[i]
-    
-                        break
-                    end
-                end
-    
-                if not current_motor then continue end
-
-                if settings['Method'] == 'Tween' or settings['Method'] == 'Lerp' then
-    
-                    local a,b = string.split(tostring(v.EasingStyle),'.'),string.split(tostring(v.EasingDirection),'.')
-    
-                    local tween_info = TweenInfo.new(
-                        settings['Speed'],
-                        Enum.EasingStyle[a[#a]],
-                        Enum.EasingDirection[b[#b]],
-                        0,false,0
-                    )
-                    
-                    if settings['Method'] == 'Tween' then
-                        (
-                            tween_service:Create(
-                                current_motor,
-                                tween_info,
-                                {Transform = v.CFrame}
-                            )
-                        ):Play()
-                    else
-                        current_motor.Transform = current_motor.Transform:lerp(
-                            v.CFrame,
-                            tween_service:GetValue(
-                                .5,
-                                Enum.EasingStyle[a[#a]],
-                                Enum.EasingDirection[b[#b]]
-                               )
-                        )  
-                    end
-                else
-                    current_motor.Transform = v.CFrame
-                end
-                
-                if not playing then break end
-            end
-        
-            if not playing then break end
-
-            swait(settings['Speed'])
-        end
-    end
-end
-
-make_draggable(drag)
-
-id_input.FocusLost:Connect(on_focus_lost_id)
-method_input.FocusLost:Connect(on_focus_lost_method)
-speed_input.FocusLost:Connect(on_focus_lost_speed)
-
-play_bttn.MouseButton1Click:Connect(play)
+return TextLabel,IDinput,ModeInput,SpeedInput,PlayButton
